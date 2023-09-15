96bdc5fccc20fb37d59cbe2da51b4dd473b827e8
inpink <inpink@kakao.com>
2023-09-15 19:54:56 +0900

    Initial commit

---
+++ src/main/java/knusearch/clear/ClearApplication.java
@@ -0,0 +1,13 @@
+package knusearch.clear;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClearApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClearApplication.class, args);
+	}
+
+}
---
+++ src/test/java/knusearch/clear/ClearApplicationTests.java
@@ -0,0 +1,13 @@
+package knusearch.clear;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClearApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
