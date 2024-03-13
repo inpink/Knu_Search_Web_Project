5372f653de66b9f0931785eea489a1ee6bf805ab
inpink <inpink@kakao.com>
2024-03-13 12:38:00 +0900

    feat: es merge

---
+++ src/main/java/knusearch/clear/elasticsearch/service/ElasticsearchService.java
@@ -0,0 +1,40 @@
+
+package knusearch.clear.elasticsearch.service;
+
+import knusearch.clear.elasticsearch.domain.BasePostElasticsearchEntity;
+import knusearch.clear.elasticsearch.repository.BasePostElasticsearchRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class ElasticsearchService { //자바를 통해서 엘라스틱서치 조작(우분투 자체로는 불가능. es 조작 툴 필요. 종류는 다양하다)
+    //우분투에서 es 켜놓고(start) http://localhost:9200/ 접속 시 es정보 뜸
+
+    private final BasePostElasticsearchRepository elasticsearchRepository;
+
+    @Transactional
+    public List<BasePostElasticsearchEntity> searchOrPosts(String query) {
+
+        return elasticsearchRepository.findByTitleOrText(query, query);
+    }
+
+    @Transactional
+    public List<BasePostElasticsearchEntity> searchAndPosts(String query) {
+
+        return elasticsearchRepository.searchByTitleOrText(query, query);
+    }
+
+    @Transactional
+    public List<BasePostElasticsearchEntity> searchAndPostWithBoostClassification(String query,
+                                                                                  String classification) {
+
+        return elasticsearchRepository.searchByTitleTextAndBoostClassification(
+                query,
+                query,
+                classification);
+    }
+}
